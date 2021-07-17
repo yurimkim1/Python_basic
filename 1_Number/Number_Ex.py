3aab32eba3f6dfad2a55cec32cff2af00435d414
YuRim Kim <78572509+yurimkim1@users.noreply.github.com>
2021-07-18 01:20:37 +0900

    Add files via upload

---
+++ 1_Number/Number_Ex.py
@@ -0,0 +1,38 @@
+# 숫자형(Number)이란 숫자형으로 이루어진 자료형
+# 정수, 실수, 8진수, 16진수
+
+# 정수형(int)
+a = 123
+print(a)
+
+a = -178
+print(a)
+
+a = 0
+print(a)
+
+# 실수형 (float)
+a = 1.2
+print(a)
+
+a = -3.45
+print(a)
+
+a = 4.24E10 # 4.24 x 10의 10승
+print(a)
+
+a = 4.24e-10 # 4.24 x 10의 -10승
+print(a)
+
+# 8진수(octal)
+# 8진수 만들기 위해서는 숫자가 0o 또는 0O(숫자0 + 알파벳 소문자 o 또는 대문자 O) 시작
+a = 0o177
+print(a)
+
+# 16진수(hexadecimal)
+# 16진수 만들기 위해서는 0x로 시작
+a = 0x8ff
+print(a)
+
+b = 0xABC
+print(a)
